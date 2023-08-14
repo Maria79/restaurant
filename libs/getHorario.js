e3bb13fa5ea37e365ed15e3d645dbc5909047111
Maria79 <mariajalbisu@gmail.com>
2023-08-14 21:47:50 +0100

    commit

--- libs/getHorario.js
+++
@@ -1,6 +0,0 @@
-export default async function getHorario() {
-	const res = await fetch('http://127.0.0.1:3000/api/horario');
-	const data = res.json();
-
-	return data;
-}
--- libs/getMenu.js
+++
@@ -1,6 +0,0 @@
-export default async function getMenu() {
-	const res = await fetch('http://127.0.0.1:3000/api/menu');
-	const data = res.json();
-	console.log(data);
-	return data;
-}
